e08e9eedbd79abd1b2fdaa0778161335d62f8bc6
Timothy Siskin <tymoteuszczyz@gmail.com>
2020-11-30 11:23:20 +0100

    First step of new prims: all cells are walls

--- src/primsMazeAlgorithm/primsMaze.js
+++ src/primsMazeAlgorithm/primsMaze.js
@@ -6,19 +6,16 @@ let end = endNode;
 let maze = [];
 let walls = [];
 
-//Random, hight number
+let recursion = 0;
+
 
 
 
 //First step of algorithm: all cells must be walls
 
-for(let value of grid){
-    for(let cells of value){
-        cells.isWall = true;
-    }
+for(let cell of grid){
+    console.log(cell)
 }
-
-
 
 //secend step: chose a first cell and add it to the maze, next add neighbors to the walls:
 
@@ -32,113 +29,106 @@ primsAlgorithm(current);
 
 //***************************************************************************************************** */
 
-function primsAlgorithm (current) {
+function primsAlgorithm (current) 
+{
 
-    maze.push(current);
+        maze.push(current);
 
-    for(let value of current.neighbors){
+        for(let value of current.neighbors){
 
-        walls.push(value)
-    
-    }
+            walls.push(value)
+        
+        }
 
-    
-    
-    
-    //third step: 
-    
-    while(walls.length > 0){
-    
-        let randomCell = Math.floor(Math.random() * (walls.length))
-        let randomWall = walls[randomCell];
+        
+        
+        
+        //third step: 
+        
+        if(walls.length > 0){
+        
 
-       
-        for(let cell of walls){
+        
+            for(let cell of walls){
 
-            //Cheking if only one cell neighbor is a path
+                //Cheking if only one cell neighbor is a path
 
-            let neighborPath = 0;
+                let neighborPath = 0;
 
-            for(let value of cell.neighbors){
-                if(value.isWall === false ){
-                    neighborPath++ 
+                for(let value of cell.neighbors){
+                    if(value.isWall === false ){
+                        neighborPath++ 
+                    }
+                    
                 }
-                
+
+                //checking if cell weight is lowest from neighbours and it has only one neighbor that is path element
+
+            if(neighborPath === 1){
+
+            cell.isWall = false;
+            
             }
 
-            //checking if cell weight is lowest from neighbours and it has only one neighbor that is path element
+            for (let values of cell.neighbors){
+                walls.push(values)
 
-           if(neighborPath === 1){
-
-           cell.isWall = false;
-           for (let values of cell.neighbors){
-               walls.push(values)
-           }
-
-           //i must pop cell from walls[]
-
-           
-
-        }
+            //i must pop cell from walls[]
 
 
+
+            }
+
+
+
+            }
 
         }
+
+        /****************************************************************************************k */
+
+            //Removing maze from walls
+            
+            for( let value of maze){
+
+                walls.splice(walls.indexOf(value), 1)
+
+            }
+
+
+            //removing duplicates from walls array, (Set object will lets you store only unique elements)
+
+            let unique = [...new Set(walls)];
+            walls = unique;
+
+            for(let value of walls){
+
+                if(value.isWall === false){
+                    walls.splice(walls.indexOf(value), 1)
+                }
+            }
+
+        /**************************************************************************************** */
+            let randomCell = Math.floor(Math.random() * (walls.length))
+            let randomWall = walls[randomCell];
+
+            if(recursion = 3){
+                return;
+            }
+            else recursion++;
+
+            
+
+            primsAlgorithm(randomWall);
+
 
     }
 
-// if (!(current.isWall && randomWall.isWall)){
-
-//     randomWall.isWall = false;
-
-
-// }
-
-
-
-    //     if((current.isWall === false && randomWall.isWall !== false) || (current.isWall !== false && randomWall.isWall === false)){
-
-            
-    //         randomWall.isWall = false;
-    //         walls = [];
-    //         for(let value of randomWall.neighbors){
-    //             walls.push(value)
-    //         }
-    
-    //         // walls = walls.filter((values) => values !== randomWall)
-    //         //         //  .filter((values) => {
-
-    //         //         //     console.log(values)
-    //         //         //     return (values.isWall === false);
-
-    //         //         //   });
-    //     }
-    
-        
-    
-        
-    
-        
-    //     recursion++
-    //     if(recursion === 25){
-
-    //         return console.log("zjebałeś")
-    //     }
-    //     primsAlgorithm(randomWall);
-    
-    // }
-
-
-
-
-
-}
-
 }
 
 
 
-//*********************************************************************** */
+
 
 
 
