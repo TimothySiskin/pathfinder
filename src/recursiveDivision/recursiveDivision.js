3e552c98113ce4e07482bf23a2774220e4ea454a
Timothy Siskin <tymoteuszczyz@gmail.com>
2020-12-14 18:01:23 +0100

    prototype of recursiveDivision maze in pseudocode

---
+++ src/recursiveDivision/recursiveDivision.js
@@ -0,0 +1,18 @@
+function recursiveDivision(grid, startNode, endNode){
+
+let maze = []
+let wals = []
+
+//1. All grid is not walls => this is a orginal chamber
+//2. divide chamber in two / more walls with single passage in dandom spot
+//3. continue until each chamber has witdh of one
+
+
+
+
+
+
+
+}
+
+export default recursiveDivision
