8bc3ab64e00d695745c8bc4bf1ec59f42cdcf40c
Timothy Siskin <tymoteuszczyz@gmail.com>
2020-12-15 15:38:34 +0100

    added gridIsWall function

--- src/recursiveDivision/recursiveDivision.js
+++ src/recursiveDivision/recursiveDivision.js
@@ -4,15 +4,28 @@ let maze = []
 let wals = []
 
 //1. All grid is not walls => this is a orginal chamber
+
+gridIsWall(grid);
+createBox(maze);
+createChamber();
+
 //2. divide chamber in two / more walls with single passage in dandom spot
 //3. continue until each chamber has witdh of one
 
 
 
 
+function gridIsWall(grid){
+    for(let cell of grid){
+        maze.push(cell)
+    }
 
+    for(let cell of maze)
+    {
+        cell.isWall = false;
+    }
+};
 
 
 }
-
 export default recursiveDivision
