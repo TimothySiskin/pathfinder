dcb0608e5a4b1f1ae692410349dd92f3763a7319
Timothy Siskin <tymoteuszczyz@gmail.com>
2021-04-22 23:07:24 +0200

    -isWall +prettier

--- src/components/node.js
+++ src/components/node.js
@@ -1,17 +1,13 @@
-import React from 'react'
-import "./node.css"
+import React from "react";
+import "./node.css";
 
-const Node = ({isStart, isEnd, row, col, isWall, weight}) => {
-    const classes = isStart 
-    ? "node-start" 
-    : isWall 
-    ? "isWall" 
-    : isEnd 
-    ? "node-end" 
-    : "";
-    return (
-        <div className={`node ${classes}`} id={`node-${col}-${row}`}>  </div>
-    )
-}
+const Node = ({ isStart, isEnd, row, col, isWall, weight }) => {
+  const classes = isStart ? "node-start" : isEnd ? "node-end" : "";
+  return (
+    <div className={`node ${classes}`} id={`node-${col}-${row}`}>
+      {" "}
+    </div>
+  );
+};
 
 export default Node;
