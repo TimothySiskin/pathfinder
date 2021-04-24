17c1ca2993f7e2903fb153192773efffb0726640
Timothy Siskin <tymoteuszczyz@gmail.com>
2021-04-24 18:11:42 +0200

    +clear button

--- src/components/buttons.jsx
+++ src/components/buttons.jsx
@@ -1,36 +1,37 @@
 import react from "react";
 
 const buttons = (props) => {
-  console.log(props);
   const {
     visualizePath,
     visualizeMaze,
     setMaze,
     mazeGen,
     cleanVisualization,
+    cleanMaze,
   } = props;
 
   async function submitHandler(e) {
     e.preventDefault();
-    await cleanVisualization();
-    await setMaze({ type: e.target[0].value });
-    await mazeGen(e.target[0].value);
-    await visualizeMaze();
+    cleanVisualization();
+    setMaze({ type: e.target[0].value });
+    mazeGen(e.target[0].value);
+    visualizeMaze();
   }
 
   return (
     <div>
       <form onSubmit={submitHandler}>
         <select name="maze-type">
-          <option value="empty" selected>
-            Empty Maze
+          <option value="recursive" selected>
+            Recursive Division Maze
           </option>
           <option value="primsMaze">Prims Maze</option>
-          <option value="recursive">Recursive Division Maze</option>
+          <option value="random">Random Maze</option>
         </select>
         <button type="submit">Create Maze</button>
       </form>
       <button onClick={visualizePath}>Visualize Path</button>
+      <button onClick={cleanMaze}>Clean Maze</button>
     </div>
   );
 };
